59e8de39320eeee18ee7da3481e60a40634cfe11
Leonardo Prado <125320363+leonardodiprado@users.noreply.github.com>
2024-04-25 21:55:24 -0300

    Add files via upload

---
+++ Lista/src/ListaEncadeada.java
@@ -0,0 +1,9 @@
+public class ListaEncadeada {
+
+    Node inicio;
+
+    public void inserir(int valor);
+    public void remover(int valor);
+    public void listar();
+
+}
---
+++ Lista/src/Node.java
@@ -0,0 +1,21 @@
+public class Node {
+
+    int valor;
+    Node proximo;
+
+    public int getValor() {
+        return valor;
+    }
+
+    public Node getProximo() {
+        return proximo;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public void setProximo(Node proximo) {
+        this.proximo = proximo;
+    }
+}
